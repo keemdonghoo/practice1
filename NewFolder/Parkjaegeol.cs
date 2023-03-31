f0daed3708389589068eebbc23de3c474e66f39a
jgp904 <jgp904@naver.com>
2023-03-31 16:13:11 +0900

    박재걸 클래스 생성

---
+++ NewFolder/Parkjaegeol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace practice1.NewFolder
+{
+    internal class Parkjaegeol
+    {
+        
+    }
+}
